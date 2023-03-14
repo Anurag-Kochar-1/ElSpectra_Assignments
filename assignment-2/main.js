0d7f9bcf5b95461fd372b86a731f720d80824820
Anurag <anuragmarketing101@gmail.com>
2023-03-14 14:10:35 +0530

    Add: getTop3 getBottom3 funcs

--- assignment-2/main.js
+++ assignment-2/main.js
@@ -15,5 +15,31 @@ function getRank(dataFile) {
 
 
 }
+// console.log(getRank(skillsData))
 
-console.log(getRank(skillsData))
+
+function getTop3(dataFile) {
+    const skills = dataFile.skills_capabilities;
+    const top_3_2019 = skills.map((skill) => skill).sort((a, b) => parseFloat(b.score_2019) - parseFloat(a.score_2019)).splice(0, 4).map((skill) => skill.name)
+    const top_3_2024 = skills.map((skill) => skill).sort((a, b) => parseFloat(b.score_2024) - parseFloat(a.score_2024)).splice(0, 4).map((skill) => skill.name)
+
+    return {
+        top_3_2019_capabilities: top_3_2019,
+        top_3_2024_capabilities: top_3_2024,
+
+    }
+}
+
+function getBottom3(dataFile) {
+    const skills = dataFile.skills_capabilities;
+    const bottom_3_2019 = skills.map((skill) => skill).sort((a, b) => parseFloat(a.score_2019) - parseFloat(b.score_2019)).splice(0, 4).map((skill) => skill)
+    const bottom_3_2024 = skills.map((skill) => skill).sort((a, b) => parseFloat(a.score_2024) - parseFloat(b.score_2024)).splice(0, 4).map((skill) => skill)
+
+    return {
+        bottom_3_2019_capabilities: bottom_3_2019,
+        bottom_3_2024_capabilities: bottom_3_2024,
+
+    }
+}
+// console.log(getBottom3(skillsData))
+
