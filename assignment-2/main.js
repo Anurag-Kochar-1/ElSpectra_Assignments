8ce743aa5715ee9b5b0137e5dd3183c3ae6d248b
Anurag <anuragmarketing101@gmail.com>
2023-03-14 18:32:27 +0530

    Add: calculateRank getSkillRank funcs

--- assignment-2/main.js
+++ assignment-2/main.js
@@ -50,3 +50,44 @@ const top3Accelerating = sortedGrowth.map(c => c).slice(0, 3);
 
 const top3Decelerating = sortedGrowth.map(c => c).slice(-3);
 // console.log(top3Decelerating)
+
+
+
+function calculateRank(percentage) {
+    if (percentage >= 90) {
+        return 'A+';
+    } else if (percentage >= 80) {
+        return 'A';
+    } else if (percentage >= 70) {
+        return 'B';
+    } else if (percentage >= 60) {
+        return 'C';
+    } else if (percentage >= 50) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+function getSkillRank() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Enter the name of the skill: ', (skillName) => {
+        const skill = skillsData.skills_capabilities.find(skill => skill.name === skillName);
+        if (skill) {
+            const currentScore = parseInt(skill.score_2019);
+            const futureScore = parseInt(skill.score_2024);
+            const percentageChange = ((futureScore - currentScore) / currentScore) * 100;
+            const rank = calculateRank(futureScore);
+            console.log(`${skillName} is expected to improve by ${percentageChange.toFixed(2)}% and reach a rank of ${rank}`);
+        } else {
+            console.log(`Skill ${skillName} not found`);
+        }
+
+        rl.close();
+    });
+}
+getSkillRank()
